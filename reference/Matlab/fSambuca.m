cd7a7363de73590551b73acfef400a99f7feadb2
Daniel Collins <jugglindan@gmail.com>
2014-10-24 09:29:00 +0800

    new reference code from Steve S

--- reference/Matlab/fSambuca.m
+++ reference/Matlab/fSambuca.m
@@ -1,7 +1,10 @@
 
 function calcspec = fSambuca(CHL,CDOM,TR,q,H,UQSubs, filtsum,wav, awater, aphy_star,d_wls,n_wls,filt)
 
-% SIOP and image acquistion parameters
+% SIOP (spectral inputs 'awater' and
+% 'aphy_star' are also SIOPs), as are the reference wavelengths noted further down...all these would eventually come from the
+% LUT/database based on the user selection or geographical location of the
+% data
 % ***************************************************
 X_ph_lambda0x = 0.00157747;
 X_tr_lambda0x = 0.0225353;
@@ -9,14 +12,16 @@ Sc = 0.0168052;
 Str = 0.00977262;
 a_tr_lambda0tr = 0.00433;
 Y = 0.878138;
-Qwater = 4.00;
 lambda0cdom=550.00;
 a_cdom_lambda0cdom = 1.0;
-PI=3.14159265;
+
+% image acquistion paramters from image/spectra metadata
+%********************************************************
 theta_air=30; %solar zenith
 offnad=10; % off nadir angle
 
-%SUBS
+%SUBSTRATES---only two in this case, which avoids the substrate loop. q is
+%then just a continious variable as the others to be estimated.
 r1=UQSubs(:,1);
 r2=UQSubs(:,2);
 
@@ -26,6 +31,8 @@ r2=UQSubs(:,2);
 thetaw = (asin(1/1.333*sin(PI/180.00*(theta_air))));
 thetao = (asin(1/1.333*sin(PI/180.00*(offnad))));
 
+% The wave lengths hardcoded here (550.00 and 546.00) are reference
+% wavelenghts thare are actually part of the user defined SIOP set
 for i=1:d_wls
 bbwater(i)= ( 0.00194/2.0 ) * ( ( 550.0/wav(i) )^4.32 );
 acdom_star(i) = a_cdom_lambda0cdom * (exp(-Sc * (wav(i) - 550.00) ) );
@@ -41,7 +48,7 @@ end
 
 
 for i=1:d_wls
-r(i) = (q * r1(i)) + ( (1-q) * r2(i) ); % substrate spectra - Will need modification for more then two spectra
+r(i) = (q * r1(i)) + ( (1-q) * r2(i) ); % Calculates total bottom reflecatnce from the two substrates and the proportion of q and (1-q)
 u(i) = bb(i) / (a(i) + bb(i));
 kappa(i) = a(i) + bb(i);
 end
@@ -66,15 +73,36 @@ end
 bandsum(1:36)=0;
  calc(1:551,1:36) = 0;
 
+%----------------------------------------------------------------------------- 
+% This is the application of the Sensor filter, turning the full spectrum
+% modelled data into the spectral response (filt) of the sensor (in this
+% case 551 1nm bands, to 36 bands). The synthetic sensor data I created for
+% my tests is 36 bands each 10nm wide with a constant repsonse over those
+% 10nm (see the shape of filt). A real sesnor will have more complex shape,
+% but the concept is the same
 for j=1:n_wls
 	for i=1:d_wls
-		calc(i,j) = rrsdp(i) * filt(i,j);
+		calc(i,j) = rrs(i) * filt(i,j);
 		bandsum(j) = bandsum(j)+ calc(i,j);
     end
 end
 
+% Filtsum is the area under each bands spectral reponse in filt, calculated
+% as in my fortran version below and in the McMC matlab code. This whole process of spectral resampling is
+% done using a native function in IDL, so I had to work out a way to do it
+% here. It may not be the most effective or efficient, and i'm not sure it
+% will work if the spectral filter (filt) is not normalised to 1 as it is
+% here.
+%filtsum(:) = 0
+% do j=1,n_wls
+% 	do i=1, d_wls
+% 		filtsum(j) = filtsum(j)+ filt(i,j)  
+% 	enddo
+% enddo
+
+
 for i=1:n_wls
  calcspec(i) = bandsum(i)/filtsum(i);
 end
-
+%--------------------------------------------------------------------------------
 
