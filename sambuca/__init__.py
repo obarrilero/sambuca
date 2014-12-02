1aef49936935c1cd08be3d4b8667a08757ca567a
Daniel Collins <col52j@csiro.au>
2014-12-02 17:15:31 +1100

    updated version to actual first value

--- sambuca/__init__.py
+++ sambuca/__init__.py
@@ -1,6 +1,10 @@
 """ Sambuca modeling system
 """
 
-# from . import sambuca
+from . import sambuca
 
-__version__ = '0.0.1'
+# Versioning: major.minor.patch
+# major: increment on a major version. Must be changed when the API changes in an imcompatible way.
+# minor: new functionality that does not break the existing API.
+# patch: bug-fixes that do not change the public API
+__version__ = '0.1.0'
