e30e571e7f274209314f4cdb6a6a1f1a405da0c8
Daniel Collins <col52j@csiro.au>
2014-12-03 19:16:34 +1100

    added coverage to unit tests

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 
 class PyTest(TestCommand):
-    # Entry point for py.test that allows 'python setup.py test'
-    # to work correctly
+    ''' Entry point for py.test that allows 'python setup.py test'
+    to work correctly
+    '''
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
