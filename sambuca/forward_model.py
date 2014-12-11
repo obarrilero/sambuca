b8454fce9a389dcd2663778d9785ceb35960e0ce
Daniel Collins <daniel.collins@csiro.au>
2014-12-11 16:58:33 +0800

    fixed long line in the forward model function. It is actually less readable :(

--- sambuca/forward_model.py
+++ sambuca/forward_model.py
@@ -75,56 +75,32 @@ def forward_model(
     atr_star = a_tr_lambda0tr * np.exp(-str_ * (wav - lambda0cdom))
     bbph_star = x_ph_lambda0x * np.power(546. / wav, y)
     bbtr_star = x_tr_lambda0x * np.power(546. / wav, y)
-    # for i=1:d_wls
-    # bbwater(i)= ( 0.00194/2.0 ) * ( ( 550.0/wav(i) )^4.32 );
-    # acdom_star(i) = a_cdom_lambda0cdom * (exp(-Sc * (wav(i) - 550.00) ) );
-    # atr_star(i) = a_tr_lambda0tr * (exp(-Str * (wav(i) - 550.00) ) );
-    # bbph_star(i) = X_ph_lambda0x * ( (546.00/wav(i))^Y );
-    # bbtr_star(i) = X_tr_lambda0x * ( (546.00/wav(i))^Y );
-    # end
 
     a = awater + chl * aphy_star + cdom * acdom_star + tr * atr_star
     bb = bbwater + chl * bbph_star + tr * bbtr_star
-    # for i=1:d_wls
-    # a(i) = awater(i) + (CHL * aphy_star(i)) + (CDOM * acdom_star(i))
-    #       + (TR * atr_star(i));
-    # bb(i) =  bbwater(i) + (CHL * bbph_star(i)) + (TR * bbtr_star(i));
-    # end
 
     # Calculates total bottom reflectance from the two substrates and the
     # proportion of q and (1-q)
-    r = q * substrate1 + (1-q) * substrate2
+    r = q * substrate1 + (1. - q) * substrate2
     u = bb / (a + bb)
     kappa = a + bb
-    # for i=1:d_wls
-    # r(i) = (q * r1(i)) + ( (1-q) * r2(i) );
-    # u(i) = bb(i) / (a(i) + bb(i));
-    # kappa(i) = a(i) + bb(i);
-    # end
 
-    du_column = 1.03 * np.power(1.0 + (2.4 * u), 0.5)
-    du_bottom = 1.04 * np.power(1.0 + (5.4 * u), 0.5)
-    # for i=1:d_wls
-    # DuColumn(i) = 1.03 * (1.00 + (2.40 * u(i)) )^0.50;
-    # DuBottom(i) = 1.04 * (1.00 + (5.40 * u(i)) )^0.50;
-    # end
+    du_column = 1.03 * np.power(1. + (2.4 * u), 0.5)
+    du_bottom = 1.04 * np.power(1. + (5.4 * u), 0.5)
 
     rrsdp = (0.084 + 0.17 * u) * u
     # TODO: Ask Steve why these are unused
     # kd = kappa * (1.0 / np.cos(thetaw))
     # kuc = kappa * (du_column / np.cos(thetao))
     # kub = kappa * (du_bottom / np.cos(thetao))
-    # for i=1:d_wls
-    # rrsdp(i) = (0.084 + (0.17 * u(i)) ) * u(i) ;
-    # Kd(i)=   kappa(i) * (1.00     / cos(thetaw));
-    # Kuc(i)=  kappa(i) * (DuColumn(i) / cos(thetao));
-    # Kub(i)=  kappa(i) * (DuBottom(i) / cos(thetao));
-    # end
 
-    rrs = rrsdp * (1.0 - np.exp(-((1.0 / np.cos(thetaw)) + (du_column / np.cos(thetao))) * kappa * h)) \
-        + ((1.0 / math.pi) * r * np.exp(-((1.0 / np.cos(thetaw)) + (du_bottom / np.cos(thetao))) * kappa * h))
-    # for i=1:d_wls
-    # rrs(i) = rrsdp(i) * (1.00 - exp(-( (1.00/cos(thetaw)) + (DuColumn(i) / cos(thetao)) )* kappa(i) * H)) + ( (1.00/PI) * r(i) * exp(-( (1.00/cos(thetaw)) + (DuBottom(i) / cos(thetao)) )* kappa(i) * H)   );
-    # end
+    inv_cos_thetaw = 1. / math.cos(thetaw)
+    du_column_scaled = du_column / math.cos(thetao)
+    du_bottom_scaled = du_bottom / math.cos(thetao)
+    kappa_h = kappa * h
+    rrs = rrsdp * \
+        (1. - np.exp(-(inv_cos_thetaw + du_column_scaled) * kappa_h)) + \
+        ((1. / math.pi) * r *
+         np.exp(-(inv_cos_thetaw + du_bottom_scaled) * kappa_h))
 
     return rrs
