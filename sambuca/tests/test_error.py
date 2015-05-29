24d7489ac9a6e05f0ab66a600895a2e594ffad11
Daniel Collins <daniel.collins@csiro.au>
2015-05-29 11:03:01 +0800

    refactored error unit tests to use the IDL data. Noise errors are failing

--- sambuca/tests/test_error.py
+++ sambuca/tests/test_error.py
@@ -7,7 +7,7 @@ from builtins import *
 
 import sambuca as sb
 import numpy as np
-from scipy.io import loadmat
+from scipy.io import readsav
 from pkg_resources import resource_filename
 
 
@@ -17,121 +17,103 @@ class TestErrorFunctions(object):
 
     def setup_method(self, method):
         # load the test values generated from the Matlab code
-        filename = resource_filename(
-            sb.__name__,
-            'tests/data/test_error_noise.mat')
-        self.__noisedata = loadmat(filename, squeeze_me=True)
+        self.noise_data = readsav(
+            resource_filename(
+                sb.__name__,
+                'tests/data/error_data.sav'))
 
-        filename = resource_filename(
-            sb.__name__,
-            'tests/data/test_error_no_noise.mat')
-        self.__data = loadmat(filename, squeeze_me=True)
+        self.no_noise_data = readsav(
+            resource_filename(
+                sb.__name__,
+                'tests/data/no_noise_error_data.sav'))
 
     @staticmethod
     def unpack_data(data):
-        return (data['observed_spectra'],
-                data['modelled_spectra'],
+        return (data['realrrs'],
+                data['rrs'],
                 data['noiserrs'],
-                data['num_bands'],
-                data['distance_alpha'],
-                data['distance_alpha_f'],
-                data['distance_f'],
-                data['distance_lsq'],
+                data['lsq'],
                 data['error_a'],
                 data['error_af'],
                 data['error_f'])
 
     def validate_data(self, data):
-        data_ = self.unpack_data(data)
-        os = data_[0]
-        ms = data_[1]
-        noise = data_[2]
-        num_bands = data_[3]
-        assert os.shape[0] == num_bands
-        assert ms.shape[0] == num_bands
+        unpacked_data = self.unpack_data(data)
+        observed = unpacked_data[0]
+        modelled = unpacked_data[1]
+        noise = unpacked_data[2]
+        num_bands = len(observed)
+        assert observed.shape[0] == num_bands
+        assert modelled.shape[0] == num_bands
         assert noise.shape[0] == num_bands
         assert noise.shape[0] == num_bands
 
     def test_validate_no_noise_data(self):
-        self.validate_data(self.__data)
+        self.validate_data(self.no_noise_data)
 
     def test_validate_noise_data(self):
-        self.validate_data(self.__noisedata)
+        self.validate_data(self.noise_data)
 
     def test_error_no_noise(self):
-        os, ms, _, _, expected_distance_alpha, expected_distance_alpha_f, \
-            expected_distance_f, expected_distance_lsq, expected_error_a, \
-            expected_error_af, expected_error_f \
-            = self.unpack_data(self.__data)
+        observed, modelled, _, expected_lsq, expected_distance_a, \
+            expected_distance_af, expected_distance_f \
+            = self.unpack_data(self.no_noise_data)
 
-        actual = sb.error_all(os, ms)
+        actual = sb.error_all(observed, modelled)
 
-        assert np.allclose(actual.distance_alpha, expected_distance_alpha)
-        assert np.allclose(actual.distance_alpha_f, expected_distance_alpha_f)
+        assert np.allclose(actual.distance_alpha, expected_distance_a)
+        assert np.allclose(actual.distance_alpha_f, expected_distance_af)
         assert np.allclose(actual.distance_f, expected_distance_f)
-        assert np.allclose(actual.distance_lsq, expected_distance_lsq)
-
-        # The IDL code was returning some identical values with different names
-        # These tests ensure that we still have access to all the required
-        # values
-        assert np.allclose(actual.distance_alpha, expected_error_a)
-        assert np.allclose(actual.distance_f, expected_error_f)
+        assert np.allclose(actual.distance_lsq, expected_lsq)
 
     def test_error_noise(self):
-        os, ms, noise, _, expected_distance_alpha, expected_distance_alpha_f, \
-            expected_distance_f, expected_distance_lsq, expected_error_a, \
-            expected_error_af, expected_error_f \
-            = self.unpack_data(self.__noisedata)
+        observed, modelled, nedr, expected_lsq, expected_distance_a, \
+            expected_distance_af, expected_distance_f \
+            = self.unpack_data(self.noise_data)
 
-        actual = sb.error_all(os, ms, noise)
+        actual = sb.error_all(observed, modelled, nedr)
 
-        assert np.allclose(actual.distance_alpha, expected_distance_alpha)
-        assert np.allclose(actual.distance_alpha_f, expected_distance_alpha_f)
+        assert np.allclose(actual.distance_alpha, expected_distance_a)
+        assert np.allclose(actual.distance_alpha_f, expected_distance_af)
         assert np.allclose(actual.distance_f, expected_distance_f)
-        assert np.allclose(actual.distance_lsq, expected_distance_lsq)
+        assert np.allclose(actual.distance_lsq, expected_lsq)
 
-        # The IDL code was returning some identical values with different names
-        # These tests ensure that we still have access to all the required
-        # values
-        assert np.allclose(actual.distance_alpha, expected_error_a)
-        assert np.allclose(actual.distance_f, expected_error_f)
+    def test_distance_alpha_noise(self):
+        observed = self.noise_data['realrrs']
+        modelled = self.noise_data['rrs']
+        nedr = self.noise_data['noiserrs']
+        expected = self.noise_data['error_a']
 
-    def test_distance_alpha(self):
-        os = self.__noisedata['observed_spectra']
-        ms = self.__noisedata['modelled_spectra']
-        nedr = self.__noisedata['noiserrs']
-        expected = self.__noisedata['distance_alpha']
-
-        actual = sb.distance_alpha(os, ms, nedr)
+        actual = sb.distance_alpha(observed, modelled, nedr)
 
         assert np.allclose(actual, expected)
 
-    def test_distance_alpha_f(self):
-        os = self.__noisedata['observed_spectra']
-        ms = self.__noisedata['modelled_spectra']
-        nedr = self.__noisedata['noiserrs']
-        expected = self.__noisedata['distance_alpha_f']
+    def test_distance_alpha_f_noise(self):
+        observed = self.noise_data['realrrs']
+        modelled = self.noise_data['rrs']
+        nedr = self.noise_data['noiserrs']
+        expected = self.noise_data['error_af']
 
-        actual = sb.distance_alpha_f(os, ms, nedr)
-
-        assert np.allclose(actual, expected)
-
-    def test_distance_f(self):
-        os = self.__noisedata['observed_spectra']
-        ms = self.__noisedata['modelled_spectra']
-        nedr = self.__noisedata['noiserrs']
-        expected = self.__noisedata['distance_f']
-
-        actual = sb.distance_f(os, ms, nedr)
+        actual = sb.distance_alpha_f(observed, modelled, nedr)
 
         assert np.allclose(actual, expected)
 
-    def test_distance_lsq(self):
-        os = self.__noisedata['observed_spectra']
-        ms = self.__noisedata['modelled_spectra']
-        nedr = self.__noisedata['noiserrs']
-        expected = self.__noisedata['distance_lsq']
+    def test_distance_f_noise(self):
+        observed = self.noise_data['realrrs']
+        modelled = self.noise_data['rrs']
+        nedr = self.noise_data['noiserrs']
+        expected = self.noise_data['error_f']
 
-        actual = sb.distance_lsq(os, ms, nedr)
+        actual = sb.distance_f(observed, modelled, nedr)
+
+        assert np.allclose(actual, expected)
+
+    def test_distance_lsq_noise(self):
+        observed = self.noise_data['realrrs']
+        modelled = self.noise_data['rrs']
+        nedr = self.noise_data['noiserrs']
+        expected = self.noise_data['lsq']
+
+        actual = sb.distance_lsq(observed, modelled, nedr)
 
         assert np.allclose(actual, expected)
