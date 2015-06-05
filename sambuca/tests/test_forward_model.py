d30980e548ca2600a9f55763aeba1c6d9c400fb5
Daniel Collins <daniel.collins@csiro.au>
2015-06-05 15:53:18 +0800

    removed extra test data file

--- sambuca/tests/test_forward_model.py
+++ sambuca/tests/test_forward_model.py
@@ -25,7 +25,7 @@ class TestForwardModel(object):
         # load the test values
         filename = resource_filename(
             sb.__name__,
-            'tests/data/forward_model_test_data.sav')
+            './tests/data/_F1nm_H25_a_Non_UQ02_MB_RC__OS_SHon.sav')
         cls.data = readsav(filename)
         cls.unpack_parameters()
         cls.unpack_input_spectra()
