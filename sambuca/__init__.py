10b9443be74df6c8687993f1e9efd6112a2b7a95
Daniel Collins <daniel.collins@csiro.au>
2015-12-21 16:23:22 +0800

    started updating reference notebooks for the new functionality. Added a FreeParameters named tuple to Sambuca for more readable optimisation code

--- sambuca/__init__.py
+++ sambuca/__init__.py
@@ -1,12 +1,15 @@
 """ Sambuca modeling system
 """
 
-from .error import \
-    error_all, \
-    distance_alpha, \
-    distance_f, \
-    distance_lsq, \
-    distance_alpha_f
+from .error import (
+    error_all,
+    distance_alpha,
+    distance_f,
+    distance_lsq,
+    distance_alpha_f,
+)
+
+from .free_parameters import FreeParameters
 
 __author__ = 'Daniel Collins'
 __email__ = 'daniel.collins@csiro.au'
